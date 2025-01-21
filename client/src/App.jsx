e8fc59b682166ba0b6ca880468d2b641b2550e19
Mohamad Masri <Mohamadmasri123>
2025-01-21 14:25:18 +0200

    edit app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,13 @@ import Register from "./pages/Register";
 export default function App() {
   return (
     <Router> 
-    <div className="bg-primary text-[#404040]">
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         
       </Routes>
-   </div>
     </Router>
   );
 }
