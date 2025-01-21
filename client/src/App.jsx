2cf724dab3de24ec6d914c7a3b8188f34467f290
Mohamad Masri <Mohamadmasri123>
2025-01-21 19:05:55 +0200

    refactor code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
- function App() {
+export default function App() {
   return (
-    <Router> 
-
+    <BrowserRouter> 
+    <div className="bg-primary text-[#404040]">
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
       </Routes>
-    </Router>
+   </div>
+    </BrowserRouter>
   );
 }
-export default App;
